5d55ce590e5736bef10919da9537ff66bba8a702
Dadzial <duszakdamian034@gmail.com>
2026-05-31 23:02:34 +0200

    add full lang service

--- src/app/core/services/translations/translations.service.ts
+++ src/app/core/services/translations/translations.service.ts
@@ -1,10 +1,35 @@
-import { Injectable } from '@angular/core';
+import { Injectable ,inject, signal} from '@angular/core';
+import { TranslateService } from '@ngx-translate/core';
+
+export type Language = 'en' | 'pl';
 
 @Injectable({
   providedIn: 'root',
 })
 export class TranslationsService {
+  private translate = inject(TranslateService);
 
-  constructor() {}
+  currentLang = signal<Language>(this.getSavedLang());
 
+  constructor() {
+    this.translate.addLangs(['pl', 'en']);
+    this.translate.setDefaultLang('pl');
+    this.translate.use(this.currentLang());
+  }
+
+  setLanguage(lang: Language) {
+    this.currentLang.set(lang);
+    this.translate.use(lang);
+    localStorage.setItem('lang', lang);
+  }
+
+  toggle() {
+    this.setLanguage(this.currentLang() === 'pl' ? 'en' : 'pl');
+  }
+
+  private getSavedLang(): Language {
+    const saved = localStorage.getItem('lang') as Language;
+    if (saved === 'pl' || saved === 'en') return saved;
+    return 'en';
+  }
 }
