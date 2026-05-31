248100f144778fb8259977f4883432377bc45413
Dadzial <duszakdamian034@gmail.com>
2026-05-31 23:09:10 +0200

    add empty lang button

---
+++ src/app/shared/components/lang-button/lang-button.component.ts
@@ -0,0 +1,9 @@
+import { Component } from '@angular/core';
+
+@Component({
+  selector: 'app-lang-button',
+  imports: [],
+  templateUrl: './lang-button.component.html',
+  styles: ``,
+})
+export class LangButtonComponent {}
