42e44a5fe3566ea618e663c9d82cb7c6f6255f47
Dadzial <duszakdamian034@gmail.com>
2026-06-01 18:05:43 +0200

    add empty theme button

---
+++ src/app/shared/components/theme-button/theme-button.component.ts
@@ -0,0 +1,9 @@
+import { Component } from '@angular/core';
+
+@Component({
+  selector: 'app-theme-button',
+  imports: [],
+  templateUrl: './theme-button.component.html',
+  styles: ``,
+})
+export class ThemeButtonComponent {}
