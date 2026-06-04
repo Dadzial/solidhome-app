939a59415871708e0f62873b413d8028bc7b8fbc
Dadzial <duszakdamian034@gmail.com>
2026-06-04 22:54:43 +0200

    add new form

---
+++ src/app/features/auth/components/register-from/register-from.component.ts
@@ -0,0 +1,9 @@
+import { Component } from '@angular/core';
+
+@Component({
+  selector: 'app-register-from',
+  imports: [],
+  templateUrl: './register-from.component.html',
+  styles: ``,
+})
+export class RegisterFromComponent {}
