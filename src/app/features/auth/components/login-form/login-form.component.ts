cdb183b6766f476b79fc02cf07b58238a5164f5e
Dadzial <duszakdamian034@gmail.com>
2026-06-04 19:35:45 +0200

    base login complete

--- src/app/features/auth/components/login-form/login-form.component.ts
+++ src/app/features/auth/components/login-form/login-form.component.ts
@@ -1,10 +1,22 @@
-import { Component } from '@angular/core';
+import { Component, signal } from '@angular/core';
+import { SvgIconComponent } from 'angular-svg-icon';
 
 @Component({
   selector: 'app-login-form',
   standalone:true,
-  imports: [],
+  imports: [SvgIconComponent],
   templateUrl: './login-form.component.html',
   styles: ``,
 })
-export class LoginFormComponent {}
+export class LoginFormComponent {
+  public authMode = signal<'login' | 'signup'>('login');
+  public rememberMe = signal<boolean>(false);
+
+  public setMode(mode: 'login' | 'signup'): void {
+    this.authMode.set(mode);
+  }
+
+  public toggleRememberMe(): void {
+    this.rememberMe.set(!this.rememberMe());
+  }
+}
--- src/app/pages/auth/auth-page.component.ts
+++ src/app/pages/auth/auth-page.component.ts
@@ -1,11 +1,12 @@
 import { Component } from '@angular/core';
 import { ThemeButtonComponent } from '@shared/components/theme-button/theme-button.component';
 import {LangButtonComponent} from '@shared/components/lang-button/lang-button.component';
+import { LoginFormComponent } from '@features/auth/components/login-form/login-form.component';
 
 @Component({
   selector: 'app-auth-page',
   standalone: true,
-  imports: [ThemeButtonComponent,LangButtonComponent],
+  imports: [ThemeButtonComponent, LangButtonComponent, LoginFormComponent],
   templateUrl: './auth-page.component.html',
   styles: ``,
 })
