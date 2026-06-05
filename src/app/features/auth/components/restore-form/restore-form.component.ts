c9ee311a8f7d0655984891e45b26ffd58380d56a
Dadzial <duszakdamian034@gmail.com>
2026-06-05 22:37:08 +0200

    add confirm passwrod  form

--- src/app/features/auth/components/restore-form/restore-form.component.ts
+++ src/app/features/auth/components/restore-form/restore-form.component.ts
@@ -1,8 +1,10 @@
 import { Component } from '@angular/core';
+import { SvgIconComponent } from 'angular-svg-icon';
+import { TranslatePipe } from '@ngx-translate/core';
 
 @Component({
   selector: 'app-restore-form',
-  imports: [],
+  imports: [SvgIconComponent, TranslatePipe],
   standalone: true,
   templateUrl: './restore-form.component.html',
   styles: ``,
