d78f6a4675c7293b6cdc038c4bf7915c4430b090
Dadzial <duszakdamian034@gmail.com>
2026-06-06 23:28:30 +0200

    add base logic to login

--- src/app/features/auth/components/login-form/login-form.component.ts
+++ src/app/features/auth/components/login-form/login-form.component.ts
@@ -1,20 +1,60 @@
-import { Component, output, signal } from '@angular/core';
+import { Component, computed, output, signal } from '@angular/core';
 import { TranslateModule } from '@ngx-translate/core';
 import { SvgIconComponent } from 'angular-svg-icon';
+import { form, FormField } from '@angular/forms/signals';
+
+
+interface LoginCredentials {
+  username: string;
+  password: string;
+  rememberMe: boolean;
+}
 
 @Component({
   selector: 'app-login-form',
   standalone: true,
-  imports: [SvgIconComponent, TranslateModule],
+  imports: [SvgIconComponent, TranslateModule, FormField],
   templateUrl: './login-form.component.html',
   styles: ``,
 })
 export class LoginFormComponent {
   public switchToSignup = output<void>();
   public switchToVerify = output<void>();
-  public rememberMe = signal<boolean>(false);
+  public isLoading = signal(false);
+
+  public loginModel = signal<LoginCredentials>({
+    username: '',
+    password: '',
+    rememberMe: false,
+  });
+
+  protected readonly loginForm = form(this.loginModel);
+
+  public isFormInvalid = computed(() => {
+    const data = this.loginModel();
+    return !data.username || !data.password;
+  });
+
 
   public toggleRememberMe(): void {
-    this.rememberMe.set(!this.rememberMe());
+    this.loginForm.rememberMe().value.update((current) => !current);
+  }
+
+  public async onSubmit(event: Event) {
+    event.preventDefault();
+
+    if (this.isFormInvalid() || this.isLoading()) return;
+
+    this.isLoading.set(true);
+
+    const credentials = this.loginModel();
+
+    try {
+      console.log('send Data to api:', credentials);
+    } catch (error) {
+      console.error('Login error:', error);
+    } finally {
+      this.isLoading.set(false);
+    }
   }
 }
