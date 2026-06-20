9b91680c516c3a15f009b41afa9b078627fa1ab7
Dadzial <duszakdamian034@gmail.com>
2026-06-20 19:30:59 +0200

    add buttons to dashboard

--- src/app/pages/dashboard/dashboard.component.ts
+++ src/app/pages/dashboard/dashboard.component.ts
@@ -1,9 +1,11 @@
 import { Component } from '@angular/core';
 import { NavbarComponent } from '@shared/components/navbar/navbar.component';
+import { ThemeButtonComponent } from '@shared/components/theme-button/theme-button.component';
+import { LangButtonComponent } from '@shared/components/lang-button/lang-button.component';
 
 @Component({
   selector: 'app-dashboard',
-  imports: [NavbarComponent],
+  imports: [NavbarComponent, LangButtonComponent, ThemeButtonComponent],
   templateUrl: './dashboard.component.html',
   styles: ``,
 })
--- src/app/shared/components/navbar/navbar.component.ts
+++ src/app/shared/components/navbar/navbar.component.ts
@@ -1,10 +1,12 @@
 import { Component , inject } from '@angular/core';
 import { TranslateModule } from '@ngx-translate/core';
 import { SvgIconComponent } from 'angular-svg-icon';
+import { LangButtonComponent } from '@shared/components/lang-button/lang-button.component';
+import { ThemeButtonComponent } from '@shared/components/theme-button/theme-button.component';
 
 @Component({
   selector: 'app-navbar',
-  imports: [TranslateModule,SvgIconComponent],
+  imports: [TranslateModule, SvgIconComponent, LangButtonComponent, ThemeButtonComponent],
   templateUrl: './navbar.component.html',
   styles: ``,
 })
