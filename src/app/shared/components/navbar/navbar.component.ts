af4699ffb5da0579f53cd3d8ef9d3210f32a8508
Dadzial <duszakdamian034@gmail.com>
2026-06-21 13:29:40 +0200

    add new translations

--- src/app/shared/components/navbar/navbar.component.ts
+++ src/app/shared/components/navbar/navbar.component.ts
@@ -1,12 +1,10 @@
 import { Component , inject } from '@angular/core';
 import { TranslateModule } from '@ngx-translate/core';
 import { SvgIconComponent } from 'angular-svg-icon';
-import { LangButtonComponent } from '@shared/components/lang-button/lang-button.component';
-import { ThemeButtonComponent } from '@shared/components/theme-button/theme-button.component';
 
 @Component({
   selector: 'app-navbar',
-  imports: [TranslateModule, SvgIconComponent, LangButtonComponent, ThemeButtonComponent],
+  imports: [TranslateModule, SvgIconComponent],
   templateUrl: './navbar.component.html',
   styles: ``,
 })
