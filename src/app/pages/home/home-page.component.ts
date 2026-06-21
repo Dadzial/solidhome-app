4f65d5cc0113d107feee1591d03c0d7f26372e58
Dadzial <duszakdamian034@gmail.com>
2026-06-21 20:37:39 +0200

    prepare for get username

--- src/app/pages/home/home-page.component.ts
+++ src/app/pages/home/home-page.component.ts
@@ -1,11 +1,12 @@
 import { Component } from '@angular/core';
+import {TranslateModule} from '@ngx-translate/core';
 import { NavbarComponent } from '@shared/components/navbar/navbar.component';
 import { ThemeButtonComponent } from '@shared/components/theme-button/theme-button.component';
 import { LangButtonComponent } from '@shared/components/lang-button/lang-button.component';
 
 @Component({
   selector: 'app-home',
-  imports: [NavbarComponent, LangButtonComponent, ThemeButtonComponent],
+  imports: [TranslateModule,NavbarComponent, LangButtonComponent, ThemeButtonComponent],
   standalone: true,
   templateUrl: './home-page.component.html',
   styles: ``,
