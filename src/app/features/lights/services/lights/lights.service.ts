9015145aa76351c278d7a8d95e5bba6813fdb035
Dadzial <duszakdamian034@gmail.com>
2026-06-28 16:53:17 +0200

    add new serice to lights

--- src/app/features/lights/services/lights/lights.service.ts
+++ src/app/features/lights/services/lights/lights.service.ts
@@ -1,6 +1,28 @@
-import { Injectable } from '@angular/core';
+import { Injectable , inject } from '@angular/core';
+import { HttpClient, HttpErrorResponse } from '@angular/common/http';
+import { environment } from '@environments/environment';
+import { catchError, throwError } from 'rxjs';
+
+
+interface LightRequest {
+  numberOfLight: number;
+  state :number
+}
+
+interface LightResponse {
+  status:number;
+}
+
+interface ApiError {
+  error: string;
+  details?: string[];
+  value?: string;
+}
 
 @Injectable({
   providedIn: 'root',
 })
-export class LightsService {}
+export class LightsService {
+  private http = inject(HttpClient);
+  private readonly apiUrl = `${environment.apiUrl}/lights`;
+}
