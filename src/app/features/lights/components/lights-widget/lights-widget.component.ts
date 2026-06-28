bfe5829a107680ec15ff3820864b5eec874e368b
Dadzial <duszakdamian034@gmail.com>
2026-06-28 16:07:22 +0200

    add png to lights widget

--- src/app/features/lights/components/lights-widget/lights-widget.component.ts
+++ src/app/features/lights/components/lights-widget/lights-widget.component.ts
@@ -1,8 +1,10 @@
 import { Component } from '@angular/core';
+import { TranslatePipe } from '@ngx-translate/core';
+import {SvgIconComponent} from 'angular-svg-icon';
 
 @Component({
   selector: 'app-lights-widget',
-  imports: [],
+  imports: [TranslatePipe,SvgIconComponent],
   templateUrl: './lights-widget.component.html',
   styles: ``,
 })
--- src/app/pages/home/home-page.component.ts
+++ src/app/pages/home/home-page.component.ts
@@ -4,10 +4,17 @@ import {LoginService} from '@features/auth/services/login/login.service';
 import { NavbarComponent } from '@shared/components/navbar/navbar.component';
 import { ThemeButtonComponent } from '@shared/components/theme-button/theme-button.component';
 import { LangButtonComponent } from '@shared/components/lang-button/lang-button.component';
+import { LightsWidgetComponent } from '@features/lights/components/lights-widget/lights-widget.component';
 
 @Component({
   selector: 'app-home',
-  imports: [TranslateModule,NavbarComponent, LangButtonComponent, ThemeButtonComponent],
+  imports: [
+    TranslateModule,
+    NavbarComponent,
+    LangButtonComponent,
+    ThemeButtonComponent,
+    LightsWidgetComponent,
+  ],
   standalone: true,
   templateUrl: './home-page.component.html',
   styles: ``,
