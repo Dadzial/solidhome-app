44b15fdbc1e7618ea7fe7ae97ad8f70e159ce33a
Dadzial <duszakdamian034@gmail.com>
2026-06-28 16:55:45 +0200

    add base of service

--- src/app/features/lights/services/lights/lights.service.ts
+++ src/app/features/lights/services/lights/lights.service.ts
@@ -25,4 +25,21 @@ interface ApiError {
 export class LightsService {
   private http = inject(HttpClient);
   private readonly apiUrl = `${environment.apiUrl}/lights`;
+
+  private turnLights(data:LightResponse){
+
+  }
+
+  private handleError(error: HttpErrorResponse) {
+    if (error.status === 400) {
+      return throwError(() => error.error as ApiError);
+    }
+    if (error.status === 401) {
+      return throwError(() => ({ error: 'Invalid email' }));
+    }
+    if (error.status === 429) {
+      return throwError(() => ({ error: 'Too many requests, try again later' }));
+    }
+    return throwError(() => ({ error: 'Server error, try again later' }));
+  }
 }
