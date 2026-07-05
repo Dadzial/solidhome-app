9023f13d80688e29c57dcb8fc545b6125a62d333
Dadzial <duszakdamian034@gmail.com>
2026-07-05 11:20:41 +0200

    add titles

--- src/app/pages/air/air-page.component.ts
+++ src/app/pages/air/air-page.component.ts
@@ -2,10 +2,11 @@ import { Component } from '@angular/core';
 import { NavbarComponent } from '@shared/components/navbar/navbar.component';
 import { ThemeButtonComponent } from '@shared/components/theme-button/theme-button.component';
 import { LangButtonComponent } from '@shared/components/lang-button/lang-button.component';
+import { TranslateModule } from '@ngx-translate/core';
 
 @Component({
   selector: 'app-air',
-  imports: [NavbarComponent, LangButtonComponent, ThemeButtonComponent],
+  imports: [NavbarComponent, LangButtonComponent, ThemeButtonComponent, TranslateModule],
   standalone: true,
   templateUrl: './air-page.component.html',
   styles: ``,
--- src/app/pages/lights/lights-page.component.ts
+++ src/app/pages/lights/lights-page.component.ts
@@ -2,10 +2,16 @@ import { Component } from '@angular/core';
 import { NavbarComponent } from '@shared/components/navbar/navbar.component';
 import { ThemeButtonComponent } from '@shared/components/theme-button/theme-button.component';
 import { LangButtonComponent } from '@shared/components/lang-button/lang-button.component';
+import { TranslateModule } from '@ngx-translate/core';
 
 @Component({
   selector: 'app-lights',
-  imports: [NavbarComponent, LangButtonComponent, ThemeButtonComponent],
+  imports: [
+    NavbarComponent,
+    LangButtonComponent,
+    ThemeButtonComponent,
+    TranslateModule,
+  ],
   standalone: true,
   templateUrl: './lights-page.component.html',
   styles: ``,
