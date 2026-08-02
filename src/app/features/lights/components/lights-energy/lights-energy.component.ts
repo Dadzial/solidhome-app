2f9ed785816b102b9c96883ccb5e19b441139ae4
Dadzial <duszakdamian034@gmail.com>
2026-08-02 15:13:30 +0200

    nowe okna

--- src/app/features/lights/components/lights-energy/lights-energy.component.ts
+++ src/app/features/lights/components/lights-energy/lights-energy.component.ts
@@ -1,8 +1,10 @@
 import { Component } from '@angular/core';
+import { TranslateModule } from '@ngx-translate/core';
+import { SvgIconComponent } from 'angular-svg-icon';
 
 @Component({
   selector: 'app-lights-energy',
-  imports: [],
+  imports: [SvgIconComponent,TranslateModule],
   standalone: true,
   templateUrl: './lights-energy.component.html',
   styles: ``,
