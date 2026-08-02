66028572d29b9f76b7ac5edb61f1f6701ee185b5
Dadzial <duszakdamian034@gmail.com>
2026-08-02 14:15:42 +0200

    puste okna

--- src/app/pages/lights/lights-page.component.ts
+++ src/app/pages/lights/lights-page.component.ts
@@ -4,6 +4,9 @@ import { ThemeButtonComponent } from '@shared/components/theme-button/theme-butt
 import { LangButtonComponent } from '@shared/components/lang-button/lang-button.component';
 import { TranslateModule } from '@ngx-translate/core';
 import { LightsWidgetComponent } from '@features/lights/components/lights-widget/lights-widget.component';
+import {LightsScenesComponent} from '@features/lights/components/lights-scenes/lights-scenes.component';
+import {LightsSchedulesComponent} from '@features/lights/components/lights-schedules/lights-schedules.component';
+import {LightsEnergyComponent} from '@features/lights/components/lights-energy/lights-energy.component';
 
 @Component({
   selector: 'app-lights',
@@ -13,6 +16,9 @@ import { LightsWidgetComponent } from '@features/lights/components/lights-widget
     LangButtonComponent,
     ThemeButtonComponent,
     LightsWidgetComponent,
+    LightsScenesComponent,
+    LightsEnergyComponent,
+    LightsSchedulesComponent
   ],
   standalone: true,
   templateUrl: './lights-page.component.html',
