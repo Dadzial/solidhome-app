8974e10ddd710b26e0b3a0883a5894145fdd6d3f
Dadzial <duszakdamian034@gmail.com>
2026-09-15 21:07:41 +0200

    js doc loading and test

--- src/app/core/services/loading/loading.service.spec.ts
+++ src/app/core/services/loading/loading.service.spec.ts
@@ -1,16 +1,39 @@
 import { TestBed } from '@angular/core/testing';
-
 import { LoadingService } from './loading.service';
 
 describe('LoadingService', () => {
   let service: LoadingService;
 
   beforeEach(() => {
-    TestBed.configureTestingModule({});
+    TestBed.configureTestingModule({
+      providers: [LoadingService],
+    });
     service = TestBed.inject(LoadingService);
   });
 
   it('should be created', () => {
     expect(service).toBeTruthy();
+  });
+
+  it('should have initial isLoading state set to false', () => {
+    expect(service.isLoading()).toBe(false);
+  });
+
+  describe('showLoadingWindow', () => {
+    it('should set isLoading to true', () => {
+      service.showLoadingWindow();
+
+      expect(service.isLoading()).toBe(true);
+    });
+  });
+
+  describe('hideLoadingWindow', () => {
+    it('should set isLoading to false after being shown', () => {
+      service.showLoadingWindow();
+      expect(service.isLoading()).toBe(true);
+
+      service.hideLoadingWindow();
+      expect(service.isLoading()).toBe(false);
+    });
   });
 });
--- src/app/core/services/loading/loading.service.ts
+++ src/app/core/services/loading/loading.service.ts
@@ -1,15 +1,31 @@
 import { Injectable , signal } from '@angular/core';
-
+/**
+ * Serwis odpowiedzialny za wywołanie loading screen w aplikacji.
+ *
+ * ### Zasady działania:
+ * - Przechowuje stan ładowania w reaktywnym sygnale `isLoading`.
+ * - Udostępnia metody do pokazywania i ukrywania okna ładowania.
+ */
 @Injectable({
   providedIn: 'root',
 })
 export class LoadingService {
+  /**
+   * Reaktywny sygnał przechowujący aktualny stan ładowania aplikacji.
+   * @type signal
+   */
   public isLoading = signal<boolean>(false);
-
+  /**
+   * Ustawia sygnał isLoading na true, przez co ekran ładowania będzie widoczny w aplikacji.
+   * @returns void
+   */
   public showLoadingWindow(): void {
     this.isLoading.set(true);
   }
-
+  /**
+   * Ustawia sygnał isLoading na false, przez co ekran ładowania nie będzie widoczny w aplikacji.
+   * @returns void
+   */
   public hideLoadingWindow(): void {
     this.isLoading.set(false);
   }
