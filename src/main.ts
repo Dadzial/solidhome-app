dd7eba62318ea0b679307139ff8295a6439869f9
Dadzial <duszakdamian034@gmail.com>
2026-09-19 20:42:31 +0200

    js doc main

--- src/main.ts
+++ src/main.ts
@@ -1,6 +1,8 @@
 import { bootstrapApplication } from '@angular/platform-browser';
 import { appConfig } from './app/app.config';
 import { App } from './app/app';
-
-bootstrapApplication(App, appConfig)
-  .catch((err) => console.error(err));
+/**
+ * Główny plik wejściowy (entry point) aplikacji SolidHomeApp.
+ * Uruchamia główny komponent aplikacji `App` z konfiguracją `appConfig`.
+ */
+bootstrapApplication(App, appConfig).catch((err) => console.error(err));
