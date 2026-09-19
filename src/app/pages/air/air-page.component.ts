a7f99387b0f027d7eb78bbfae6cab6b5742703d9
Dadzial <duszakdamian034@gmail.com>
2026-09-19 19:49:00 +0200

    air page test

--- src/app/pages/air/air-page.component.ts
+++ src/app/pages/air/air-page.component.ts
@@ -3,7 +3,9 @@ import { NavbarComponent } from '@shared/components/navbar/navbar.component';
 import { ThemeButtonComponent } from '@shared/components/theme-button/theme-button.component';
 import { LangButtonComponent } from '@shared/components/lang-button/lang-button.component';
 import { TranslateModule } from '@ngx-translate/core';
-
+/**
+ * Komponent strony reprezentujacy informacje o jakości powietrza (w rozwoju)
+ */
 @Component({
   selector: 'app-air',
   imports: [NavbarComponent, LangButtonComponent, ThemeButtonComponent, TranslateModule],
