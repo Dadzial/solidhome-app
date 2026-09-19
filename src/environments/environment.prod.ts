d6a15f80ec0447e844d95d3100240069c35e1eb0
Dadzial <duszakdamian034@gmail.com>
2026-09-19 20:43:44 +0200

    env jsdoc

--- src/environments/environment.prod.ts
+++ src/environments/environment.prod.ts
@@ -1,6 +1,11 @@
+/**
+ * Konfiguracja środowiska produkcyjnego aplikacji.
+ */
 export const environment = {
+  /** Flaga określająca, czy aplikacja działa w trybie produkcyjnym. */
   production: true,
+  /** Obiekt konfiguracji połączeń z API w środowisku produkcyjnym. */
   api: {
-    url: '', // empty for this moment
+    url: ''
   },
 };
--- src/environments/environment.ts
+++ src/environments/environment.ts
@@ -1,4 +1,9 @@
+/**
+ * Konfiguracja środowiska deweloperskiego aplikacji.
+ */
 export const environment = {
+  /** Flaga określająca, czy aplikacja działa w trybie produkcyjnym. */
   production: false,
+  /** Adres bazowy API w środowisku deweloperskim. */
   apiUrl: 'http://localhost:3100/api',
-}
+};
