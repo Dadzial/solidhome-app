b4907569a103702eab9ebe314f00b270be80b9c2
Dadzial <duszakdamian034@gmail.com>
2026-09-20 11:22:50 +0200

    new header

--- src/app/features/lights/components/lights-enegry/lights-energy.component.ts
+++ src/app/features/lights/components/lights-enegry/lights-energy.component.ts
@@ -1,8 +1,10 @@
 import { Component } from '@angular/core';
+import { SvgIconComponent } from 'angular-svg-icon';
+import { TranslatePipe } from '@ngx-translate/core';
 
 @Component({
   selector: 'app-lights-energy',
-  imports: [],
+  imports: [SvgIconComponent, TranslatePipe],
   templateUrl: './lights-energy.component.html',
   styles: ``,
 })
